053ac832281c0180d79c28c9f9ac30d96f9b9c9e
JichiSenpai <tammi_574@hotmail.com>
2016-01-19 19:18:43 +0200

    tehtävä 11 valmis

---
+++ Tehtavat1/Tehtavat1/Valmiit/Harjoitus11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtavat1
+{
+    class Harjoitus11
+    {
+        static void Main(string[] args)
+        {
+            int uinput;
+
+            Console.Write("Anna luku > ");
+            uinput = int.Parse(Console.ReadLine()) + 1;
+
+            for (int i=1; i<uinput; i++)
+            {
+                for (int j=0; j<i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
