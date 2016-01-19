a856f8a1e3d297cb6464c30de2afc9a703974d3e
JichiSenpai <tammi_574@hotmail.com>
2016-01-19 18:26:27 +0200

    commit messagee oon kirjoitellu

---
+++ Tehtavat1/Tehtavat1/Valmiit/Harjoitus7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtavat1
+{
+    class Harjoitus7
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            
+
+
+            Console.Write("Anna vuosi > ");
+            year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 100 != 0 | year % 400 == 0)
+            {
+                Console.Write("Vuosi " + year + " on karkausvuosi.");
+                Console.ReadLine();
+            }
+            else {
+                Console.Write("Vuosi " + year + " ei ole karkausvuosi.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
