80b2442b30185ed56c50bbc6019193f557c8aa3a
JichiSenpai <tammi_574@hotmail.com>
2016-01-19 18:55:45 +0200

    tehtävä 9 tehty

---
+++ Tehtavat1/Tehtavat1/Valmiit/Harjoitus9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtavat1
+{
+    class Harjoitus9
+    {
+        static int kokoluku;
+        static void Main(string[] args)
+        {
+            
+            bool mainloop;
+
+            mainloop = true;
+
+            while (mainloop)
+            {
+                Console.Write("Anna luku > ");
+                int templuku = int.Parse(Console.ReadLine());
+                if (templuku == 0)
+                {
+                    break;
+                }
+                else {
+                    kokoluku = kokoluku + templuku;
+                }
+            }
+
+            Console.Write("Lukujen summa on " + kokoluku);
+            Console.Read();
+        }
+    }
+}
