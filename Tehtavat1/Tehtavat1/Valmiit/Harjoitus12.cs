421490be60bc02ff863b31349dbd074e3c468918
JichiSenpai <tammi_574@hotmail.com>
2016-01-19 19:30:55 +0200

    tehtävä 12 tehty

---
+++ Tehtavat1/Tehtavat1/Valmiit/Harjoitus12.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtavat1
+{
+    class Harjoitus12
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut;
+            luvut = new int[5];
+            int loop;
+            loop = 0;
+
+            while (loop != 5)
+            {
+                Console.Write("Anna luku > ");
+                luvut[loop] = int.Parse(Console.ReadLine());
+                loop++;
+            }
+
+            Console.Write("\n");
+            loop--;
+
+            while (loop > -1)
+            {
+                Console.Write(luvut[loop]);
+                loop--;
+            }
+            Console.Write("\n");
+
+        }
+    }
+}
