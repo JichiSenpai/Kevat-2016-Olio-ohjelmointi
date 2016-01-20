05918faa0d32cd0bda3de75038296bf626207340
Jaakko Tammela <tammi_574@hotmail.com>
2016-01-20 04:46:02 +0200

    tehtäviä

---
+++ Tehtavat1/Tehtavat1/Valmiit/Harjoitus16.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtavat1
+{
+    class Harjoitus16
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int uinput;
+            int randluku = rnd.Next(0, 100);
+            int kerrat = 0;
+            bool looppi = true;
+
+            while (looppi) {
+                Console.Write("Arvaa luku > ");
+                uinput = int.Parse(Console.ReadLine());
+                if (uinput > randluku)
+                {
+                    Console.Write("Luku on pienempi\n");
+                    kerrat++;
+                }
+                if (uinput < randluku)
+                {
+                    Console.Write("Luku on suurempi\n");
+                    kerrat++;
+                }
+                if (uinput == randluku)
+                {
+                    Console.Write("Arvasit luvun!\n");
+                    kerrat++;
+                    looppi = false;
+                    break;
+                }
+            }
+
+            Console.Write("Arvasit luvun " + kerrat + " kerralla\n");
+        }
+    }
+}
---
+++ Tehtavat1/Tehtavat1/Valmiit/Harjoitus18.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtavat1
+{
+    class Harjoitus18
+    {
+        static void Main(string[] args)
+        {
+            string uinput;
+            char[] stringcpy;
+            string inputcpy;
+
+            Console.Write("Anna lause > ");
+            uinput = Console.ReadLine();
+
+            stringcpy = uinput.ToCharArray();
+            Array.Reverse(stringcpy);
+
+            inputcpy = new string(stringcpy);
+            Console.WriteLine("Antamasi lauseesi: " + uinput);
+            Console.WriteLine("Antamasi lause takaperin: " + inputcpy);
+        
+            if (uinput == inputcpy)
+            {
+                Console.WriteLine("Antamasi lause on palindromi");
+            }
+        }
+    }
+}
