81a9322528edd4ce8a536928eef8b2319ab5c4cc
JichiSenpai <tammi_574@hotmail.com>
2016-01-27 19:01:59 +0200

    demo5

---
+++ Demo5/Demo5/Harjoitus1/MainProg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo5.Harjoitus1
+{
+    class MainProg
+    {
+        static void Main(string[] args)
+        {
+            Vehicle mycar = new Vehicle("Toyota", "asdasd123");
+            Console.WriteLine(mycar.ToString());
+
+            //mycar.tyres,Add(new Tyre("asd", "asd"));
+            mycar.RemoveTyres();
+            Console.WriteLine(mycar.ToString());
+
+            mycar.AddTyre(new Tyre("Nokia", "123123", "asdasd"));
+            Console.WriteLine(mycar.ToString());
+
+
+        }
+    }
+}
---
+++ Demo5/Demo5/Harjoitus1/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo5.Harjoitus1
+{
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        
+        public void RemoveTyres() {
+            tyres.Clear();
+        }
+
+        public void AddTyre(Tyre t)
+        {
+            tyres.Add(t);
+        }
+
+        public Vehicle(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+
+            tyres = new List<Tyre>();
+
+            AddTyre(new Tyre("Nokia", "asdasd123", "asdasd123"));
+            AddTyre(new Tyre("Nokia", "asdasd123", "asdasd123"));
+            AddTyre(new Tyre("Nokia", "asdasd123", "asdasd123"));
+            AddTyre(new Tyre("Nokia", "asdasd123", "asdasd123"));
+        }
+
+        public override string ToString()
+        {
+            string s = "Vehicle: " + Brand + ", " + Model;
+
+            foreach(Tyre t in tyres)
+            {
+                s += "\n";
+                s += t.ToString();
+            }
+
+            return s;
+        }
+        
+        private List<Tyre> tyres;
+    }
+}
