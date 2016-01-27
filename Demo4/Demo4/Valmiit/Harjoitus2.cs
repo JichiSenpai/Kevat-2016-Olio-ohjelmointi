6b311dc5eea06914e938c81fc48c6a19eb66572f
JichiSenpai <tammi_574@hotmail.com>
2016-01-27 17:52:16 +0200

    asdasd

---
+++ Demo4/Demo4/Valmiit/Harjoitus2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo4
+{
+    class Styrkkari
+    {
+        private readonly int maxVolume = 100;
+        private readonly int minVolume = 0;
+
+        private int volume;
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume) volume = value;
+                else
+                {
+                        value = maxVolume;
+                }
+                if (value >= minVolume) volume = value;
+                else
+                {
+                        volume = minVolume;
+                }
+            }
+        }
+
+        public Styrkkari()
+        {
+
+        }
+
+        public Styrkkari(int uinput_volume)
+        {
+            Volume = uinput_volume;
+        }
+    }
+    class Harjoitus2
+    {
+        static void Main(string[] args)
+        {
+            Styrkkari vahvari = new Styrkkari();
+
+            bool looppi = true;
+
+            while (looppi)
+            {
+                Console.Write("Give a new volume value> ");
+                vahvari.Volume = int.Parse(Console.ReadLine());
+                Console.WriteLine("-> Volume set to: " + vahvari.Volume);
+            }
+        }
+    }
+}
