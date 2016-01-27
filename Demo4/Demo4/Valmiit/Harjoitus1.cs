de416f41623ccca3dccf88d62c6d3ddd15301dff
JichiSenpai <tammi_574@hotmail.com>
2016-01-27 17:22:48 +0200

    shittii

---
+++ Demo4/Demo4/Valmiit/Harjoitus1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo4
+{
+    class Hissi
+    {
+        private readonly int maxFloors = 5;
+        private readonly int minFloors = 1;
+        
+        private int Floor;
+        private int oldFloor;
+        public int floor{
+            get
+            {
+                return Floor;
+            }
+            set
+            {
+                if (value > maxFloors)
+                {
+                    Console.WriteLine("Invalid floor. Too high.");
+                    value = oldFloor;
+                }
+                if (value < minFloors)
+                {
+                    Console.WriteLine("Invalid floor. Too low.");
+                    value = oldFloor;
+                }
+                else
+                {
+                    Floor = value;
+                    oldFloor = value;
+                }
+            }
+        }
+        
+        public Hissi()
+        {
+
+        }
+
+        public Hissi(int uinput_floor)
+        {
+            Floor = uinput_floor;
+        }
+    }
+    class Harjoitus1
+    {
+        static void Main(string[] args)
+        {
+            Hissi dynamo = new Hissi();
+
+            bool looppi = true;
+
+            while(looppi)
+            {
+                Console.Write("Give a new floor number> ");
+                dynamo.floor = int.Parse(Console.ReadLine());
+                Console.WriteLine("Elevator is now in floor: " + dynamo.floor);
+            }
+        }
+    }
+}
