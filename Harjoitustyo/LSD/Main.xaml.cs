5b385aa2ac4a80cdd58a1fd1be2bad8ab0f06ea3
Jaakko Tammela <K1697@student.jamk.fi>
2016-04-25 01:53:51 +0300

    aaaaa

--- Harjoitustyo/LSD/Main.xaml.cs
+++ Harjoitustyo/LSD/Main.xaml.cs
@@ -25,10 +25,23 @@ namespace LSD
         public int regionId { get; set; }
         public int platformId { get; set; }
 
+        public static int gamecount;
+
         public Game()
         {
-
+            gamecount++;
         }
+
+        ~Game()
+        {
+            gamecount--;
+        }
+
+        public static int GetGameCount()
+        {
+            return gamecount;
+        }
+
         public Game(int gameid, string gamename, int region, int platform)
         {
             gameId = gameId;
@@ -50,47 +63,63 @@ namespace LSD
             this.InitializeComponent();
             
 
-            Game SWE1R = new Game(
+            Game game1 = new Game(
                 1,
                 "Star Wars: Episode 1 Racer",
                 1, 
                 3);
 
-            Game DMC3HD = new Game(
+            Game game2 = new Game(
                2,
                "Sly Cooper and the Thievius Raccoonus",
                2,
                2);
 
-            Game SCATR = new Game(
+            Game game3 = new Game(
                 3,
-                "Sly Cooper and the Thievius Raccoonus",
+                "Devil May Cry 3 HD",
                 3,
                 1);
 
-           
+            Dictionary<int, Game> games = new Dictionary<int, Game>();
 
-            textBlock.Text = SWE1R.gameName;
+            games.Add(1, game1);
+            games.Add(2, game2);
+            games.Add(3, game3);
+
+
 
             //<Border BorderBrush="Black" BorderThickness="0,1,0,0" Height="100">
+            
+            
+            //textBlock.Text = game1.gameName;
             int teksti = 1;
 
-            Border border = new Border();
-            border.Name = "border" + teksti;
-            border.BorderThickness = new Thickness(0, 5, 0, 0);
-
+            
             //border.BorderBrush = new SolidColorBrush((Color)color.GetValue(null));
             //new SolidColorBrush(Color.Black)
 
-            TextBlock txt = new TextBlock();
-            txt.Name = "textBox" + teksti;
-            txt.Text = SCATR.gameName;
-            txt.FontSize = 30;
-            txt.TextWrapping = TextWrapping.Wrap;
-            teksti++;
+            int looppi = games.Count;
+            
+            while (looppi != 0)
+            {
+                Border border = new Border();
+                border.Name = "border" + teksti;
+                border.BorderThickness = new Thickness(0, 5, 0, 0);
 
-            stack1.Children.Add(border);
-            border.Child = txt;
+                TextBlock txt = new TextBlock();
+                txt.Name = "textBox" + teksti;                
+                txt.Text = games[teksti].gameName;
+                txt.FontSize = 30;
+                txt.TextWrapping = TextWrapping.Wrap;
+                teksti++;
+                stack1.Children.Add(border);
+                border.Child = txt;
+                looppi--;
+            }
+
+
+
             //    stack1.Children.Add(txt);
         }
     }
